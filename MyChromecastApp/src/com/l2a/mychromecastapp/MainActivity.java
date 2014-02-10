c91ce2565f158b93332dd60ccdd705da3c80cf2e
Kevin Dyer <kevin.m.dyer@gmail.com>
2014-02-10 15:03:14 -0700

    Modified app to use action bar support libraries

--- MyChromecastApp/src/com/l2a/mychromecastapp/MainActivity.java
+++ MyChromecastApp/src/com/l2a/mychromecastapp/MainActivity.java
@@ -2,9 +2,10 @@ package com.l2a.mychromecastapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
